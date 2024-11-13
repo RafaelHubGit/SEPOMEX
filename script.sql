e0d746ce02323a73dfc9a56da6fd89b3a59bfdbe
RafaelHubGit <45270696+RafaelHubGit@users.noreply.github.com>
2024-11-13 10:22:04 -0600

    Update script.sql
    Se actualiza para agregar ciudad y las claves de inegi

--- script.sql
+++ script.sql
@@ -28,24 +28,70 @@ FROM CatSepomex cs
 ) AS abcd;
 
 
+-- Tipo Asentamiento
+CREATE TABLE CatTipoAsentamiento(
+	IdTipoAsentamiento INT PRIMARY KEY IDENTITY(1,1),
+	TipoAsentamiento VARCHAR(50) NOT NULL,
+);
+
+INSERT INTO CatTipoAsentamiento
+SELECT d_tipo_asenta 
+FROM CatSepomex cs
+GROUP BY d_tipo_asenta 
+ORDER BY d_tipo_asenta;
+
+
+-- Zona (rural, urbana, semiurbano)
+CREATE TABLE CatZonas (
+    IdZona INT PRIMARY KEY IDENTITY(1,1),
+    Zona VARCHAR(50) NOT NULL
+);
+
+INSERT INTO CatZonas (Zona)
+SELECT DISTINCT d_zona
+FROM CatSepomex
+WHERE d_zona IS NOT NULL AND d_zona != ''
+ORDER BY d_zona;
+
 -- ESTADOS
 CREATE TABLE CatEstados (
   IdEstado INT PRIMARY KEY IDENTITY(1,1),
-  Estado VARCHAR(100) NOT NULL
+  Estado VARCHAR(100) NOT NULL,
+  ClaveInegi VARCHAR(2) NOT NULL
 );
 
-SET IDENTITY_INSERT CatEstados OFF;
+-- SET IDENTITY_INSERT CatEstados OFF;
 
-INSERT INTO CatEstados (IdEstado, Estado)
+INSERT INTO CatEstados (ClaveInegi, Estado)
 SELECT DISTINCT c_estado, d_estado
 FROM CatSepomex
 ORDER BY c_estado;
 
-SET IDENTITY_INSERT CatEstados ON;
+-- SET IDENTITY_INSERT CatEstados ON;
 
 
 
+-- CIUDADES 
+CREATE TABLE CatCiudades (
+    IdCiudad INT PRIMARY KEY IDENTITY(1,1),
+    Ciudad VARCHAR(100) NOT NULL,
+    IdEstado INT NOT NULL,
+    CONSTRAINT UQ_CiudadEstado UNIQUE (Ciudad, IdEstado), -- Unicidad por ciudad y estado
+    CONSTRAINT FK_CatCiudades_CatEstados FOREIGN KEY (IdEstado) REFERENCES CatEstados(IdEstado)
+);
 
+INSERT INTO CatCiudades (Ciudad, IdEstado)
+SELECT DISTINCT 
+    d_ciudad, 
+    ce.IdEstado
+FROM 
+    CatSepomex cs
+INNER JOIN 
+    CatEstados ce ON cs.d_estado = ce.Estado 
+WHERE 
+    d_ciudad IS NOT NULL
+AND d_ciudad != ' '
+ORDER BY IdEstado, d_ciudad;
 
 
 
@@ -53,22 +99,36 @@ SET IDENTITY_INSERT CatEstados ON;
 CREATE TABLE CatMunicipios (
   IdMunicipio INT PRIMARY KEY IDENTITY(1,1),
   Municipio VARCHAR(100) NOT NULL,
+  ClaveInegi VARCHAR(3) NOT NULL,
   IdEstado INT NOT NULL,
-  CONSTRAINT FK_CatMunicipios_CatEstados FOREIGN KEY (IdEstado) REFERENCES CatEstados(IdEstado)
+  IdCiudad INT NULL,
+  CONSTRAINT FK_CatMunicipios_CatEstados FOREIGN KEY (IdEstado) REFERENCES CatEstados(IdEstado),
+  CONSTRAINT FK_CatMunicipios_CatCiudades FOREIGN KEY (IdCiudad) REFERENCES CatCiudades(IdCiudad)
 );
 
-SET IDENTITY_INSERT CatMunicipios OFF;
+-- SET IDENTITY_INSERT CatMunicipios OFF;
 
-INSERT INTO CatMunicipios (Municipio, IdEstado)
+INSERT INTO CatMunicipios (Municipio, ClaveInegi, IdEstado, IdCiudad) -- Se insertan los municipios
 SELECT DISTINCT
     cs.D_mnpio AS Municipio,
-    ce.IdEstado AS IdEstado
+    cs.c_mnpio AS ClaveInegi,
+    ce.IdEstado AS IdEstado,
+    null
 FROM
     CatSepomex cs
 INNER JOIN
     CatEstados ce ON cs.d_estado = ce.Estado
 WHERE
-    cs.D_mnpio IS NOT NULL;
+    cs.D_mnpio IS NOT NULL
+ORDER BY IdEstado, Municipio, ClaveInegi;
+
+UPDATE cm -- Se actualizan las ciudades en el municipio
+SET cm.IdCiudad = cc.IdCiudad
+FROM CatMunicipios cm
+INNER JOIN CatSepomex cs ON cm.Municipio = cs.D_mnpio
+INNER JOIN CatEstados ce ON cs.d_estado = ce.Estado
+INNER JOIN CatCiudades cc ON cs.d_ciudad = cc.Ciudad AND ce.IdEstado = cc.IdEstado
+WHERE cs.d_ciudad IS NOT NULL AND cs.d_ciudad != ' ';
 
 
 
@@ -77,21 +137,55 @@ CREATE TABLE CatColonias (
   IdColonia INT PRIMARY KEY IDENTITY(1,1),
   Colonia VARCHAR(100) NOT NULL,
   IdMunicipio INT NOT NULL,
-  CONSTRAINT FK_CatColonias_CatMunicipios FOREIGN KEY (IdMunicipio) REFERENCES CatMunicipios(IdMunicipio)
+  IdTipoAsentamiento INT NULL,
+  IdZona INT NOT NULL,
+  CONSTRAINT FK_CatColonias_CatMunicipios FOREIGN KEY (IdMunicipio) REFERENCES CatMunicipios(IdMunicipio),
+  CONSTRAINT FK_CatColonias_CatTipoAsentamiento FOREIGN KEY (IdTipoAsentamiento) REFERENCES CatTipoAsentamiento(IdTipoAsentamiento),
+  CONSTRAINT FK_CatColonias_CatZonas FOREIGN KEY (IdZona) REFERENCES CatZonas(IdZona)
 );
 
-INSERT INTO CatColonias (Colonia, IdMunicipio)
+-- Insertar colonias en CatColonias con un JOIN adicional para el tipo de asentamiento
+INSERT INTO CatColonias (Colonia, IdMunicipio, IdTipoAsentamiento, IdZona)
 SELECT DISTINCT
     cs.d_asenta AS Colonia,
-    cm.IdMunicipio AS IdMunicipio
+    cm.IdMunicipio AS IdMunicipio,
+    null, -- Se agrega como null ya que al intentar agregarlo en la consulta duplicaba algunos registros
+    cz.IdZona
 FROM
     CatSepomex cs
 INNER JOIN
     CatMunicipios cm ON cs.D_mnpio = cm.Municipio
 INNER JOIN
     CatEstados ce ON cs.d_estado = ce.Estado AND cm.IdEstado = ce.IdEstado
+-- INNER JOIN
+--     CatTipoAsentamiento cta ON cs.d_tipo_asenta = cta.TipoAsentamiento
+INNER JOIN
+    CatZonas cz ON cs.d_zona = cz.Zona
 WHERE
-    cs.d_asenta IS NOT NULL;
+    cs.d_asenta IS NOT NULL
+ORDER BY
+    cm.IdMunicipio, cs.d_asenta;
+
+
+UPDATE
+	cc
+SET
+	cc.IdTipoAsentamiento = cta.IdTipoAsentamiento
+FROM
+	CatColonias cc
+INNER JOIN CatSepomex cs ON
+	cc.Colonia = cs.d_asenta
+INNER JOIN CatTipoAsentamiento cta ON
+	cta.TipoAsentamiento = cs.d_tipo_asenta
+INNER JOIN CatMunicipios cm ON
+	cc.IdMunicipio = cm.IdMunicipio
+	AND cs.D_mnpio = cm.Municipio
+INNER JOIN CatEstados ce ON
+	cm.IdEstado = ce.IdEstado
+	AND cs.d_estado = ce.Estado
+WHERE cc.IdTipoAsentamiento IS NULL;
+
+
 
 
 --CODIGOS POSTALES
@@ -115,31 +209,37 @@ INNER JOIN
 INNER JOIN
     CatEstados ce ON cs.d_estado = ce.Estado AND cm.IdEstado = ce.IdEstado
 WHERE
-    cs.d_codigo IS NOT NULL;
+    cs.d_codigo IS NOT NULL
+    AND cs.d_codigo != ''
+ORDER BY IdColonia, CodigoPostal;
    
  
 
-   
+-- QUERY para hacer la busqueda por codigo postal
 SELECT 
-    cp.CodigoPostal,
-    col.Colonia,
-    mun.Municipio,
-    est.Estado
+  ccp.CodigoPostal,
+  cc.Colonia,
+  cm.Municipio,
+  ce.Estado,
+  ccz.Zona,
+  cta.TipoAsentamiento,
+  ci.Ciudad
 FROM 
-    CatCodigosPostales cp
+  CatCodigosPostales ccp
 INNER JOIN 
-    CatColonias col ON cp.IdColonia = col.IdColonia
+  CatColonias cc ON ccp.IdColonia = cc.IdColonia
 INNER JOIN 
-    CatMunicipios mun ON col.IdMunicipio = mun.IdMunicipio
+  CatMunicipios cm ON cc.IdMunicipio = cm.IdMunicipio
 INNER JOIN 
-    CatEstados est ON mun.IdEstado = est.IdEstado
+  CatEstados ce ON cm.IdEstado = ce.IdEstado
+INNER JOIN 
+  CatZonas ccz ON cc.IdZona = ccz.IdZona
+INNER JOIN 
+  CatTipoAsentamiento cta ON cc.IdTipoAsentamiento = cta.IdTipoAsentamiento
+INNER JOIN 
+  CatCiudades ci ON cm.IdCiudad = ci.IdCiudad
 WHERE 
-    cp.CodigoPostal = '55712'; -- 55712
-   
-   
-SELECT *
-FROM CatSepomex cs
-WHERE cs.d_asenta LIKE '%Tabac%';
+  ccp.CodigoPostal = '57300';
 
 
 
